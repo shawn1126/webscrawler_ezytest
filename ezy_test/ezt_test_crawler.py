e3424e5a8990f749a34032bb0afd844fab0ee11d
shawn908304105770311071426 <53845962+shawn908304105770311071426@users.noreply.github.com>
2021-04-12 12:14:33 +0800

    Add files via upload

---
+++ ezy_test/ezt_test_crawler.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows.
+
+# Python program to demonstrate
+# selenium
+
+from selenium import webdriver
+from time import sleep
+driver=webdriver.Chrome('./chromedriver.exe')
+driver.get('https://easytest.yzu.edu.tw')
+searchbar=driver.find_element_by_class_name('navbar-left')
+login_button=searchbar.find_element_by_tag_name('a')
+login_button.click()
+
+sleep(1)
+account=driver.find_element_by_name('cust_id')
+password=driver.find_element_by_name('cust_pass')
+account.send_keys("s1071426")
+password.send_keys("Oct19961126")
+
+driver.find_element_by_id("send").click()
